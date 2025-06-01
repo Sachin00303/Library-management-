71c1499d6552ab8f08d3bd1d859578cd5006e1ee
Sachinrathod0303 <sachin.rathod@xsavlab.com>
2025-06-01 23:40:15 +0530

    allowd all hoste

--- Library_API/settings.py
+++ Library_API/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%98)7cwbf^qhdx+1k==db^2s!$@!o^okkj2yuncp54rf5&+bcg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
